173dd6aea9d940a5117202f9a7017824dbfaba6a
BVRAJASEKHAR <venkatarajasekhar.bhuma@gmail.com>
2024-10-09 22:36:26 +0530

    Changes in the controller

--- sampleproject/src/main/java/com/example/bhuma/controller/HelloController.java
+++ sampleproject/src/main/java/com/example/bhuma/controller/HelloController.java
@@ -2,17 +2,13 @@ package com.example.bhuma.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RestController
 public class HelloController {
 
 	@GetMapping("/hello")
 	public String check() {
 		return "welcome to Spring Boot and start implementing the busines logic";
 	}
-	@GetMapping("/helloworld")
-	public String checking() {
-		return "welcome to Spring Boot application";
-	}
-
 }
