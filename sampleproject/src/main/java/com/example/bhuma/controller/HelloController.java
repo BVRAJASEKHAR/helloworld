c2a504bf79ec3332531c1988480226ffb375eb90
BVRAJASEKHAR <venkatarajasekhar.bhuma@gmail.com>
2024-10-10 12:54:05 +0530

    Added one more method in Controller class

--- sampleproject/src/main/java/com/example/bhuma/controller/HelloController.java
+++ sampleproject/src/main/java/com/example/bhuma/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.bhuma.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +9,10 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String check() {
 		return "welcome to Spring Boot and start implementing the busines logic";
+	}
+
+	@GetMapping("/helloworld")
+	public String checking() {
+		return "welcome to Spring Boot Application";
 	}
 }
