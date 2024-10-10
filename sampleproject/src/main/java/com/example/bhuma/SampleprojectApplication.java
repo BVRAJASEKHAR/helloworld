9859ae0c482de2e7b5ec878bbf9324a45b0d76a2
BVRAJASEKHAR <venkatarajasekhar.bhuma@gmail.com>
2024-10-10 12:49:02 +0530

    Modified the code in Main class

--- sampleproject/src/main/java/com/example/bhuma/SampleprojectApplication.java
+++ sampleproject/src/main/java/com/example/bhuma/SampleprojectApplication.java
@@ -2,11 +2,8 @@ package com.example.bhuma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.bhuma.controller")
 public class SampleprojectApplication {
 
 	public static void main(String[] args) {
